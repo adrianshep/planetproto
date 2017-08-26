59a2e37c3be7b4b93021fbfd2db0a9ed4b7833c1
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-08-26 09:28:45 -0400

    10th exercise in planetproto

--- function.js
+++ function.js
@@ -3,6 +3,40 @@
 
 /*
 
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    function Robot() {
+        this.parts = [];
+    }
+
+    Robot.prototype.capabilities = [];
+
+    var robby = new Robot();
+    var cranky = new Robot();
+
+    claim(robby.parts, []);
+    claim(cranky.parts, []);
+    claim(robby.capabilities, []);
+    claim(cranky.capabilities, []);
+
+    robby.parts.push('core');
+    robby.capabilities.push('fly');
+
+    claim(robby.parts, ['core']);
+    claim(cranky.parts, []);
+    claim(robby.capabilities, ['fly']);
+    claim(cranky.capabilities, ['fly']);
+
+    module.exports = {
+        Robot:  Robot,
+        robby:  robby,
+        cranky: cranky
+    }
+
+────────────────────────────────────────────────────────────────────────────────
+
+
 100 Function prototype
 Exercise 10 of 10
 
